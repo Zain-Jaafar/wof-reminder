4f9219182f67dde1e32298daab29bddb2bfb3658
Zain Jaafar <zainjaafar120@gmail.com>
2026-01-11 08:33:27 +1300

    preparing to add auth

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,6 +5,19 @@ export default defineSchema({
   waitlist: defineTable({
     name: v.string(),
     email: v.string(),
-    createdAt: v.optional(v.number()),
   }).index("by_email", ["email"]),
+  u: defineTable({
+    name: v.string(),
+    email: v.string(),
+    subscription: v.string(),
+  }).index("by_email", ["email"]),
+  vehicles: defineTable({
+    userId: v.string(),
+    clientName: v.string(),
+    clientPhoneNumber: v.string(),
+    make: v.string(),
+    plateNumber: v.string(),
+    expiryDate: v.number(),
+    reminderInterval: v.number(),
+  }).index("by_plateNumber", ["plateNumber"]),
 });
--- convex/u.ts
+++ convex/u.ts
@@ -1,24 +1,25 @@
 import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
-export const add = mutation({
+export const createUser = mutation({
   args: {
     name: v.string(),
     email: v.string(),
   },
   handler: async (ctx, args) => {
     const existing = await ctx.db
-      .query("waitlist")
+      .query("u")
       .withIndex("by_email", (q) => q.eq("email", args.email))
       .first();
 
     if (existing) {
-      throw new Error("Email already on waitlist");
+      throw new Error("There is already a user with that email");
     }
 
-    await ctx.db.insert("waitlist", {
+    await ctx.db.insert("u", {
       name: args.name,
       email: args.email,
+      subscription: "free",
     });
   },
 });
